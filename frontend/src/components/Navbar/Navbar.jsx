95126889eabdd7672266c7cdb0aae378b95e659b
vidhusan12 <vidhusan709@gmail.com>
2025-09-30 17:32:40 +1300

    Add basic Navbar component and styles

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css"
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-title">Mini AI App Builder</div>
+      <button className="navbar-btn">Get Started</button>
+    </nav>
+  );
+}
+
+export default Navbar
