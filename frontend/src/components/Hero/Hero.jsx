53d56203da72ed81c62d23bbf6eaff2e3555afeb
vidhusan12 <vidhusan709@gmail.com>
2025-10-01 22:30:35 +1300

    Fixed the two buttons

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import "./Hero.css";
+import { useNavigate } from 'react-router-dom';
 
 const Hero = () => {
+  const navigate = useNavigate()
+
+  const handleStartBuilding = () => {
+    navigate("/build")
+  }
   return (
     <section className='hero'>
       <h1 className='hero-title'>Welcome to the Mini AI App Builder</h1>
@@ -9,7 +15,7 @@ const Hero = () => {
         Quickly generate powerful AI apps by describing your idea in just a few words. Give it a try and see how easy app creation can be!
       </p>
       <div className="hero-btns">
-        <button className='hero-btn primary'>Start Building</button>
+        <button className='hero-btn primary' onClick={handleStartBuilding}>Start Building</button>
       </div>
     </section>
   )
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./Navbar.css"
+import { useNavigate } from "react-router-dom";
 
 
 const Navbar = ({showButton = true, title ="Mini AI App Builder"}) => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate("/build")
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-title">{title}</div>
-      {showButton && <button className="navbar-btn">Get Started</button>}
+      {showButton && <button className="navbar-btn" onClick={handleClick}>Get Started</button>}
     </nav>
   );
 }
